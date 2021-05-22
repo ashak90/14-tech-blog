2ac581d74e9c38e38d8800d5cfc732eaae645202
Asha Kumar <ashakumar90@gmail.com>
2021-05-21 20:17:06 -0400

    setup the home routes)

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
-const comentRoutes = require('./commentRoutes');
+const commentRoutes = require('./commentRoutes');
 
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-router.use('/comments', comentRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
