22a366ce9d3a6670f883a86d3a243c559faf4229
Asha Kumar <ashakumar90@gmail.com>
2021-05-21 21:22:13 -0400

    created all the m odels

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const Post = require('../models/Post');
+const Comment = require('../models/Comment');
 
 router.get("/", async (req, res) => {
     try {
